07d1090c7bd72170d5c8126ec7b2949c338e1ded
Hossam-Shehadeh <hossam.h.shehadeh@gmail.com>
2024-07-18 12:04:02 +0300

    Time: 66 ms (55.36%) | Memory: 53 MB (52.66%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let targetIndex = nums.length-1;
+
+    for(let i = nums.length-2 ;i>=0 ;i--){
+        if(i + nums[i] >= targetIndex ){
+            targetIndex =i;
+        }
+    }
+    return targetIndex==0;
+};
