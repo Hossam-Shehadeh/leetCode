65a940e4437b6347b6440093001326e4d25d8775
Hossam Shehadeh <hossam.h.shehadeh@gmail.com>
2024-07-29 06:47:10 +0300

    Time: 81 ms (16.45%) | Memory: 56.5 MB (31.33%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.js
@@ -0,0 +1,15 @@
+var isSubtree = function(root, subRoot) {
+    
+    function isSameTree(tree1, tree2) {
+        if (!tree1 && !tree2) return true;
+        if (!tree1 || !tree2) return false;
+        if (tree1.val !== tree2.val) return false;
+        return isSameTree(tree1.left, tree2.left) && isSameTree(tree1.right, tree2.right);
+    }
+
+    
+    if (!root) return false;
+    if (isSameTree(root, subRoot)) return true;
+    
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
