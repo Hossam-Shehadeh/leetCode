0c342b0631596885d64f9638b320eb480cca1370
Hossam Shehadeh <hossam.h.shehadeh@gmail.com>
2024-08-05 08:03:31 +0300

    Time: 408 ms (93.49%) | Memory: 82.1 MB (73.94%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.js
+++ 295-find-median-from-data-stream/find-median-from-data-stream.js
@@ -1,46 +1,125 @@
-class MedianFinder {
+var MedianFinder = function() {
+    this.low = new MaxHeap(); 
+    this.high = new MinHeap(); 
+};
+
+
+MedianFinder.prototype.addNum = function(num) {
+    if (this.low.size() === 0 || num <= this.low.peek()) {
+        this.low.add(num);
+    } else {
+        this.high.add(num);
+    }
+
+    if (this.low.size() > this.high.size() + 1) {
+        this.high.add(this.low.remove());
+    } else if (this.high.size() > this.low.size()) {
+        this.low.add(this.high.remove());
+    }
+};
+
+MedianFinder.prototype.findMedian = function() {
+    if (this.low.size() > this.high.size()) {
+        return this.low.peek();
+    } else {
+        return (this.low.peek() + this.high.peek()) / 2;
+    }
+};
+
+class MaxHeap {
     constructor() {
-        this.small = [];  
-        this.large = []; 
+        this.heap = [];
     }
-
-    addNum(num) {
-       
-        if (this.small.length === 0 || num <= this.small[this.small.length - 1]) {
-            this._binaryInsert(this.small, num);
-        } else {
-            this._binaryInsert(this.large, num);
+    add(val) {
+        this.heap.push(val);
+        this._heapifyUp(this.heap.length - 1);
+    }
+    remove() {
+        if (this.heap.length === 0) return null;
+        const root = this.heap[0];
+        const last = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this._heapifyDown(0);
         }
-
-        if (this.small.length > this.large.length + 1) {
-            this.large.unshift(this.small.pop());
-        } else if (this.large.length > this.small.length) {
-            this.small.push(this.large.shift());
+        return root;
+    }
+    peek() {
+        return this.heap[0];
+    }
+    size() {
+        return this.heap.length;
+    }
+    _heapifyUp(index) {
+        const parentIndex = Math.floor((index - 1) / 2);
+        if (index > 0 && this.heap[index] > this.heap[parentIndex]) {
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            this._heapifyUp(parentIndex);
         }
     }
+    _heapifyDown(index) {
+        const leftChild = 2 * index + 1;
+        const rightChild = 2 * index + 2;
+        let largest = index;
 
-    findMedian() {
-        if (this.small.length === this.large.length) {
-            return (this.small[this.small.length - 1] + this.large[0]) / 2;
-        } else {
-            return this.small[this.small.length - 1];
+        if (leftChild < this.heap.length && this.heap[leftChild] > this.heap[largest]) {
+            largest = leftChild;
+        }
+        if (rightChild < this.heap.length && this.heap[rightChild] > this.heap[largest]) {
+            largest = rightChild;
+        }
+        if (largest !== index) {
+            [this.heap[index], this.heap[largest]] = [this.heap[largest], this.heap[index]];
+            this._heapifyDown(largest);
         }
     }
+}
 
-   
-    _binaryInsert(arr, num) {
-        let left = 0;
-        let right = arr.length;
-
-        while (left < right) {
-            const mid = Math.floor((left + right) / 2);
-            if (arr[mid] < num) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+    add(val) {
+        this.heap.push(val);
+        this._heapifyUp(this.heap.length - 1);
+    }
+    remove() {
+        if (this.heap.length === 0) return null;
+        const root = this.heap[0];
+        const last = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this._heapifyDown(0);
         }
+        return root;
+    }
+    peek() {
+        return this.heap[0];
+    }
+    size() {
+        return this.heap.length;
+    }
+    _heapifyUp(index) {
+        const parentIndex = Math.floor((index - 1) / 2);
+        if (index > 0 && this.heap[index] < this.heap[parentIndex]) {
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            this._heapifyUp(parentIndex);
+        }
+    }
+    _heapifyDown(index) {
+        const leftChild = 2 * index + 1;
+        const rightChild = 2 * index + 2;
+        let smallest = index;
 
-        arr.splice(left, 0, num);
+        if (leftChild < this.heap.length && this.heap[leftChild] < this.heap[smallest]) {
+            smallest = leftChild;
+        }
+        if (rightChild < this.heap.length && this.heap[rightChild] < this.heap[smallest]) {
+            smallest = rightChild;
+        }
+        if (smallest !== index) {
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            this._heapifyDown(smallest);
+        }
     }
 }
