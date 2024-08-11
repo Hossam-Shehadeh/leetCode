c01786df2e3bb15085a70b77e082c41beebac8ee
Hossam Shehadeh <hossam.h.shehadeh@gmail.com>
2024-08-12 01:30:59 +0300

    Time: 47 ms (81.84%) | Memory: 48.5 MB (87.24%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.js
+++ 213-house-robber-ii/house-robber-ii.js
@@ -1,21 +1,23 @@
-var rob = function(nums) {
-    const robLinear = (houses) => {
-        let prev1 = 0, prev2 = 0;
-        for (let num of houses) {
-            let temp = Math.max(prev1, prev2 + num);
-            prev2 = prev1;
-            prev1 = temp;
-        }
-        return prev1;
-    };
+function robLinear(houses) {
+    let n = houses.length;
+    if (n === 0) return 0;
+    if (n === 1) return houses[0];
+    let dp = new Array(n).fill(0);
+    dp[0] = houses[0];
+    dp[1] = Math.max(houses[0], houses[1]);
+    for (let i = 2; i < n; i++) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + houses[i]);
+    }
+    return dp[n - 1];
+}
 
+var rob = function(nums) {
     let n = nums.length;
     if (n === 1) return nums[0];
     if (n === 2) return Math.max(nums[0], nums[1]);
 
-    let max1 = robLinear(nums.slice(1));
+    let maxExcludeFirst = robLinear(nums.slice(1));
+    let maxExcludeLast = robLinear(nums.slice(0, n - 1));
 
-    let max2 = robLinear(nums.slice(0, n - 1));
-
-    return Math.max(max1, max2);
+    return Math.max(maxExcludeFirst, maxExcludeLast);
 };
