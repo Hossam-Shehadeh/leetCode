588269277d1b8dbf62168b0a30420139c47da1e2
Hossam Shehadeh <hossam.h.shehadeh@gmail.com>
2024-08-19 21:49:00 +0300

    Time: 451 ms (94.76%) | Memory: 105.7 MB (52.42%) - LeetSync

--- 146-lru-cache/lru-cache.js
+++ 146-lru-cache/lru-cache.js
@@ -1,53 +1,26 @@
-class ListNode {
-    constructor(key, value) {
-        this.key = key;
-        this.value = value;
-        this.prev = null;
-        this.next = null;
+class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.cache = new Map();
+    }
+
+    get(key) {
+        if (!this.cache.has(key)) {
+            return -1;
+        }
+        const value = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, value);
+        return value;
+    }
+
+    put(key, value) {
+        if (this.cache.has(key)) {
+            this.cache.delete(key);
+        } else if (this.cache.size >= this.capacity) {
+            const oldestKey = this.cache.keys().next().value;
+            this.cache.delete(oldestKey);
+        }
+        this.cache.set(key, value);
     }
 }
-
-var LRUCache = function(capacity) {
-    this.capacity = capacity;
-    this.cache = new Map(); 
-    this.head = new ListNode(0, 0);
-    this.tail = new ListNode(0, 0); 
-    this.head.next = this.tail;
-    this.tail.prev = this.head;
-};
-
-LRUCache.prototype.get = function(key) {
-    if (this.cache.has(key)) {
-        let node = this.cache.get(key);
-        this._remove(node);
-        this._add(node);
-        return node.value;
-    }
-    return -1; 
-};
-
-LRUCache.prototype.put = function(key, value) {
-    if (this.cache.has(key)) {
-        this._remove(this.cache.get(key));
-    }
-    let node = new ListNode(key, value);
-    this._add(node);
-    this.cache.set(key, node);
-    if (this.cache.size > this.capacity) {
-        let lru = this.head.next;
-        this._remove(lru);
-        this.cache.delete(lru.key);
-    }
-};
-
-LRUCache.prototype._add = function(node) {
-    node.prev = this.tail.prev;
-    node.next = this.tail;
-    this.tail.prev.next = node;
-    this.tail.prev = node;
-};
-
-LRUCache.prototype._remove = function(node) {
-    node.prev.next = node.next;
-    node.next.prev = node.prev;
-};
