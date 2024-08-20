39cd86ce6b3f229026815161e2a3bccfaec81a1a
Hossam Shehadeh <hossam.h.shehadeh@gmail.com>
2024-08-20 08:33:02 +0300

    Time: 88 ms (99.32%) | Memory: 63.8 MB (82.43%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.js
@@ -0,0 +1,18 @@
+var compressedString = function(word) {
+    let comp = '';
+    let i = 0;
+
+    while (i < word.length) {
+        let char = word[i];
+        let count = 0;
+
+        while (i < word.length && word[i] === char && count < 9) {
+            count++;
+            i++;
+        }
+
+        comp += count + char;
+    }
+
+    return comp;
+};
